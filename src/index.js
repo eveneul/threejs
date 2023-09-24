073191b6f6714368672bc9abe7222c399f5e7c3e
Sky <nebulass@naver.com>
2023-09-24 23:03:16 +0900

    텍스쳐 추가

--- src/index.js
+++ src/index.js
@@ -18,38 +18,62 @@ if (WEBGL.isWebGLAvailable()) {
   const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
   renderer.setSize(window.innerWidth, window.innerHeight)
 
-  // 도넛 생성
+  // 텍스쳐 추가
 
-  const torusGeometry1 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const torusGeometry2 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const torusGeometry3 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const torusGeometry4 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
-  const torusGeometry5 = new THREE.TorusGeometry(0.3, 0.15, 16, 40)
+  const textureLoader = new THREE.TextureLoader()
+  const textureBaseColor = textureLoader.load(
+    '../public/images/texture/stone-basecolor.jpg'
+  )
+  const textureNormalColor = textureLoader.load(
+    '../public/images/texture/stone-normal.jpg'
+  )
+  const textureHeightColor = textureLoader.load(
+    '../public/images/texture/stone-height.png'
+  )
+  const textureRoughnessColor = textureLoader.load(
+    '../public/images/texture/stone-roughness.jpg'
+  )
 
-  const meterial = new THREE.MeshStandardMaterial({ color: '#ff7f00' })
-  const meteria2 = new THREE.MeshPhysicalMaterial({
-    color: '#ff7f00',
-    clearcoat: 1,
-    clearcoatRoughness: 0.3,
+  console.log(textureRoughnessColor)
+
+  // 구 생성
+  const geometry = new THREE.SphereGeometry(0.3, 32, 16)
+  const material01 = new THREE.MeshStandardMaterial({ map: textureBaseColor })
+  const material02 = new THREE.MeshStandardMaterial({
+    map: textureBaseColor,
+    normalMap: textureNormalColor,
   })
-  const toru1 = new THREE.Mesh(torusGeometry1, meterial)
-  const toru2 = new THREE.Mesh(torusGeometry2, meteria2)
-  const toru3 = new THREE.Mesh(torusGeometry3, meterial)
-  const toru4 = new THREE.Mesh(torusGeometry4, meterial)
-  const toru5 = new THREE.Mesh(torusGeometry5, meterial)
-  camera.position.z = 3
+  const material03 = new THREE.MeshStandardMaterial({
+    map: textureBaseColor,
+    normalMap: textureNormalColor, // 그림자 부분을 추가
+    displacementMap: textureHeightColor, // 밝고 어두운 부분을 높낮이를 처리
+    displacementScale: 0.07,
+  })
+  const material04 = new THREE.MeshStandardMaterial({
+    map: textureBaseColor,
+    normalMap: textureNormalColor, // 그림자 부분을 추가
+    displacementMap: textureHeightColor, // 밝고 어두운 부분을 높낮이를 처리
+    displacementScale: 0.07,
+    roughnessMap: textureRoughnessColor, // 조금 더 디테일한 질감 처리
+    roughness: 1,
+  })
 
-  toru1.position.x = -2
-  toru2.position.x = -1
-  toru3.position.x = 0
-  toru4.position.x = 1
-  toru5.position.x = 2
+  const obj01 = new THREE.Mesh(geometry, material01)
+  const obj02 = new THREE.Mesh(geometry, material02)
+  const obj03 = new THREE.Mesh(geometry, material03)
+  const obj04 = new THREE.Mesh(geometry, material04)
+
+  obj01.position.x = -2
+  obj02.position.x = -1
+  obj03.position.x = 0
+  obj04.position.x = 1
 
   // light
   const pointLight = new THREE.PointLight('#fff', 1)
   pointLight.position.set(0, 2, 12)
 
-  scene.add(toru1, toru2, toru3, toru4, toru5, pointLight)
+  scene.add(obj01, obj02, obj03, obj04, pointLight)
+  camera.position.z = 3
 
   // 애니메이션화 적용
   function render(time) {
